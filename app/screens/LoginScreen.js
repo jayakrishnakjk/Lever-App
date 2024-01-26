653d7084e51b23c760570e377f29c5bbddd40824
Waquas Raza <thewaquas@gmail.com>
2024-01-26 14:22:02 +0530

    form validation added

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -7,11 +7,19 @@ import {
 	TouchableOpacity,
 } from 'react-native';
 
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+
+const validationSchema = Yup.object().shape({
+	username: Yup.string().required().label('Username'),
+	password: Yup.string().required().label('Password'),
+});
+
 const LoginScreen = () => {
 	return (
 		<View style={styles.container}>
 			{/* logo and titles */}
-			<View style={{ alignItems: 'center', marginTop: 80 }}>
+			<View style={{ alignItems: 'center' }}>
 				<Image
 					style={{ width: 70, height: 90, marginBottom: 10 }}
 					source={require('../../assets/lever-auto-logo.png')}
@@ -27,73 +35,105 @@ const LoginScreen = () => {
 			</View>
 
 			{/* login form */}
-			<View style={{ marginTop: 40 }}>
-				<TextInput
-					placeholder="Username"
-					placeholderTextColor="#867A91"
-					style={{
-						fontSize: 18,
-						paddingHorizontal: 10,
-						height: 40,
-						// paddingVertical: 10,
-						borderWidth: 1,
-						borderColor: '#867A91',
-						marginBottom: 10,
-						borderRadius: 10,
-					}}
-				/>
+			<Formik
+				initialValues={{ username: '', password: '' }}
+				onSubmit={(val) => console.log(val)}
+				validationSchema={validationSchema}
+			>
+				{({
+					handleChange,
+					handleSubmit,
+					handleBlur,
+					values,
+					errors,
+				}) => (
+					<View style={{ marginTop: 40 }}>
+						<TextInput
+							onChange={handleChange('username')}
+							onBlur={handleBlur('username')}
+							placeholder="Username"
+							placeholderTextColor="#867A91"
+							value={values.username}
+							style={{
+								borderColor: '#867A91',
+								borderRadius: 10,
+								borderWidth: 1,
+								fontSize: 18,
+								height: 40,
+								marginBottom: errors.username ? 5 : 20,
+								paddingHorizontal: 10,
+							}}
+						/>
+						{errors.username && (
+							<Text style={{ color: 'red', marginBottom: 10 }}>
+								{errors.username}
+							</Text>
+						)}
+						{/* <Text
+							onPress={() =>
+								console.log('Forgot username clicked')
+							}
+							style={{
+								color: '#243b7f',
+								fontSize: 14,
+								alignSelf: 'flex-end',
+								marginBottom: 16,
+							}}
+						>
+							Forgot User Name?
+						</Text> */}
 
-				<Text
-					onPress={() => console.log('Forgot username clicked')}
-					style={{
-						color: '#243b7f',
-						fontSize: 14,
-						alignSelf: 'flex-end',
-						marginBottom: 16,
-					}}
-				>
-					Forgot User Name?
-				</Text>
-
-				<TextInput
-					placeholder="Password"
-					placeholderTextColor="#867A91"
-					secureTextEntry
-					style={{
-						fontSize: 18,
-						paddingHorizontal: 10,
-						height: 40,
-						// paddingVertical: 10,
-						borderWidth: 1,
-						borderColor: '#867A91',
-						marginBottom: 10,
-						borderRadius: 10,
-					}}
-				/>
-				<Text
-					onPress={() => console.log('orgot password clicked')}
-					style={{
-						color: '#243b7f',
-						fontSize: 14,
-						alignSelf: 'flex-end',
-						marginBottom: 16,
-					}}
-				>
-					Forgot Password?
-				</Text>
-
-				<TouchableOpacity
-					style={{
-						backgroundColor: '#243B7F',
-						height: 36,
-						borderRadius: 10,
-						justifyContent: 'center',
-						alignItems: 'center',
-					}}
-				>
-					<Text style={{ color: '#ffffff' }}>Sign in</Text>
-				</TouchableOpacity>
-			</View>
+						<TextInput
+							onChangeText={handleChange('password')}
+							onBlur={handleBlur('password')}
+							placeholderTextColor="#867A91"
+							placeholder="Password"
+							secureTextEntry
+							value={values.password}
+							style={{
+								fontSize: 18,
+								paddingHorizontal: 10,
+								height: 40,
+								// paddingVertical: 10,
+								borderWidth: 1,
+								borderColor: '#867A91',
+								marginBottom: errors.password ? 5 : 20,
+								borderRadius: 10,
+							}}
+						/>
+						{errors.password && (
+							<Text style={{ color: 'red', marginBottom: 10 }}>
+								{errors.password}
+							</Text>
+						)}
+						{/* <Text
+							onPress={() =>
+								console.log('orgot password clicked')
+							}
+							style={{
+								color: '#243b7f',
+								fontSize: 14,
+								alignSelf: 'flex-end',
+								marginBottom: 16,
+							}}
+						>
+							Forgot Password?
+						</Text> */}
+						<TouchableOpacity
+							onPress={handleSubmit}
+							style={{
+								backgroundColor: '#243B7F',
+								height: 36,
+								borderRadius: 10,
+								justifyContent: 'center',
+								alignItems: 'center',
+							}}
+						>
+							<Text style={{ color: '#ffffff' }}>Sign in</Text>
+						</TouchableOpacity>
+					</View>
+				)}
+			</Formik>
 		</View>
 	);
 };
@@ -103,7 +143,8 @@ export default LoginScreen;
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		marginTop: 30,
+		// marginTop: 30,
+		justifyContent: 'center',
 		paddingHorizontal: 20,
 	},
 });
