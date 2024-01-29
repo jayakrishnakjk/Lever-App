81338f1ad039029cb724ef9eb7c2c92afe79a2c4
Waquas Raza <thewaquas@gmail.com>
2024-01-29 13:04:07 +0530

    Loading issue while restarting the app

--- app/navigation/Nav.js
+++ app/navigation/Nav.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, ActivityIndicator } from 'react-native';
 import {
 	MD3LightTheme as DefaultTheme,
 	PaperProvider,
 } from 'react-native-paper';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { isLoggedInAsync } from '../redux/authSlice';
 import AppNav from './AppNav';
 import AuthNav from './AuthNav';
 
 const Nav = () => {
 	const token = useSelector((state) => state.auth.token);
 	const isLoading = useSelector((state) => state.auth.isLoading);
+
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(isLoggedInAsync());
+	}, []);
 
 	if (isLoading) {
 		return (
